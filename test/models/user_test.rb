93e8204cb60de3a2cf84fd6c5467c2146a223cd3
meikaik <meikaikoh@gmail.com>
2017-03-16 21:45:38 +0000

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -96,4 +96,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not meikai.following?(archer)
   end
   
+  test "feed should have the right posts" do
+    meikai = users(:meikai)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert meikai.feed.include?(post_following)
+    end
+    # Posts from self
+    meikai.microposts.each do |post_self|
+      assert meikai.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not meikai.feed.include?(post_unfollowed)
+    end
+  end
+  
 end
